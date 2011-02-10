71b08abafd9195153a8ccc23e042c29bed5c8454
Sam Hart <criswellious@gmail.com>
2011-02-09 22:29:27 -0800

    searching for autossh

---
+++ satorlib/__init__.py
@@ -0,0 +1,9 @@
+# Search paths
+__search_paths =
+[
+    "/bin",
+    "/usr/bin",
+    "/usr/local/bin",
+]
+
+__version__ = '0.0.1'
--- satorlib/local.py
+++ satorlib/local.py
@@ -1,5 +1,20 @@
+import os
+import sys
+
+from satorlib import *
+
 def local(config, argv):
     '''
     Local path
     '''
-    print "Foo"
+    # First, find the autossh binary
+    autossh = None
+    for path in __search_paths:
+        if os.path.isfile("%s/autossh" % path):
+            autossh = "%s/autossh" % path
+            break
+
+    if not autossh:
+        print "autossh not found on system!"
+        print "Please install autossh and try again..."
+        sys.exit()
