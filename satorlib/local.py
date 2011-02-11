70321e9ad5331e90188e344b203c51b242defb9e
Sam Hart <criswellious@gmail.com>
2011-02-11 14:37:11 -0800

    should be greater than zero

--- satorlib/local.py
+++ satorlib/local.py
@@ -41,7 +41,7 @@ def local(config, root, argv):
             machine_ids = []
             for entry in handler.all_remote:
                 machine_ids.append(entry)
-        if len(machine_ids) > 1:
+        if len(machine_ids) > 0:
             for sysname in machine_ids:
                 if handler.autossh_running[sysname]:
                     print "Connection '%s' already running! Skipping..." % sysname
